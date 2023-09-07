cbcff8610ebdad1f6ce1bedbd0463c931a5929ca
Jp3d <jptoutant3d@gmail.com>
2023-09-07 18:13:24 -0400

    New code for polling and it works

--- Programming/XC8Application1/XC8Application1/main.c
+++ Programming/XC8Application1/XC8Application1/main.c
@@ -2,8 +2,7 @@
  * main.c
  *	
  *  Created: 1er Septembre 2023
- *  Author: JP Toutant
- * 	Modified by: Jérémy JBDD
+ *  Author: JP Toutant et Jérémy JBDD
  */ 
 
 
@@ -11,99 +10,44 @@
 #include <avr/io.h>
 #define F_CPU 1000000UL
 #include <util/delay.h>
-#include <avr/interrupt.h>
 
-unsigned int door1 = 0;
-unsigned int door2 = 0;
+unsigned int window1 = 0;
+unsigned int window2 = 0;
 
-// Function prototypes
-void initInterrupts();
+void initIOports(){
 
-int main(void) {
-	
-	// Configure PC5 as a debug output
-	DDRC |= (1 << PC5);
-	
-	// Initialize external interrupts
-	initInterrupts();
+	// Set PC4 and PC5 as output pin for debug
+	DDRC |= (1 << 5);
+	DDRC |= (1 << 4);
 
-	// Enable global interrupts
-	sei();
+	// Set PD3 and PD4 as input pins
+    DDRD &= ~(1 << 3);
+    DDRD &= ~(1 << 4);
 
-	// Main loop
-	while (1) {
-		
-		if(door1 == 1){
-			// Turn on the LED (set PD2 high)
-			PORTC |= (1 << PC5);
+    // Enable pull-up resistors on PD3 and PD4
+    //PORTD |= (1 << 3);
+    //PORTD |= (1 << 4);
+}
+
+// changement de stratégie nous laissons tomber les interrupts pour simplement faire un polling
+int main(void){
+	initIOports();
+	while(1){
+		//read the state of the doors at pin 3 and 4
+		window1 = PIND & (1 << 3);
+		window2 = PIND & (1 << 4);
+		//if the door1 is open set PC5 pin to high otherwise set it to low
+		if(window1 == 0){
+			PORTC |= (1 << 5);
+		}else{
+			PORTC &= ~(1 << 5);
 		}
-		else{
-			// Turn off the LED (clear PD2)
-			PORTC &= ~(1 << PC5);			
+		if(window2 == 0){
+			PORTC |= (1 << 4);
+		}else{
+			PORTC &= ~(1 << 4);
 		}
-
-		if (door2 == 1) {
-            // Turn on the LED (set PC5 high)
-            PORTC |= (1 << PC5);
-        } else {
-            // Turn off the LED (clear PC5)
-            PORTC &= ~(1 << PC5);
-        }
-	}
-
+		_delay_ms(100);
+		}
 	return 0;
-}
-
-void initInterrupts() {
-	// Configure PD2 (INT0) and PD3 (INT1) as inputs
-	DDRD &= ~((1 << PD2) | (1 << PD3));
-	//input pullup
-	 PORTD |= (1 << PD2);  // Enable pull-up resistor (set the
-
-	// Enable external interrupts for INT0 and INT1
-	EIMSK |= (1 << INT0) | (1 << INT1);
-
-	// Interrupt configs
-	
-	// For example, to trigger on falling edges:
-	//EICRA |= (1 << ISC01) | (1 << ISC11);
-	
-	// Configure interrupt triggering logic for status changes (both edges)
-	//EICRA |= (1 << ISC00) | (1 << ISC01) | (1 << ISC10) | (1 << ISC11);
-	
-	//setup for low level trigger
-	EICRA &= ~((1 << ISC21) | (1 << ISC20) | (1 << ISC31) | (1 << ISC30));
-
-	// Clear any pending interrupts
-	EIFR = (1 << INTF0) | (1 << INTF1);
-}
-
-// Interrupt service routines
-ISR(INT0_vect) {
-	// This code executes when INT0 (PD2) is triggered
-	// Handle the door detector on PD2 here
-	
-	//flip the boolean value of door1
-	if(door1 ==1){
-		door1 = 0;
-	}
-	else
-	{
-		door1 = 1;
-	}
-	
-}
-
-ISR(INT1_vect) {
-	// This code executes when INT1 (PD3) is triggered
-	// Handle the door detector on PD3 here
-	
-	//flip the boolean value of door1
-	if(door1 ==1){
-		door2 = 0;
-	}
-	else
-	{
-		door2 = 1;
-	}
 }
