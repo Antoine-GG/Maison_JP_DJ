cc2601941b87d0f4482a9949b939cf45bd9e8d13
Jp3d <jptoutant3d@gmail.com>
2023-09-15 15:33:27 -0400

    LT_update

--- Programming/LT/LT/main.c
+++ Programming/LT/LT/main.c
@@ -5,12 +5,15 @@
 #include <util/twi.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <avr/>
 
 #define SLAVE_ADDRESS_1 0x30 // Adresse de l'ATmega328P esclave 1
 #define SLAVE_ADDRESS_2 0x08 // Adresse de l'ATmega328P esclave 2
+#define MAX_I2C_RETRIES 3
 
 volatile uint8_t windowState1 = 0; // État de la fenêtre de l'esclave 1
 volatile uint8_t windowState2 = 0; // État de la fenêtre de l'esclave 2
+int iteration =0;
 
 void i2c_init() {
 	// SDA (PC4) and SCL (PC5) are configured as inputs with pull-up resistors.
@@ -23,8 +26,25 @@ void i2c_init() {
 }
 
 void i2c_start() {
-	TWCR = (1 << TWSTA) | (1 << TWEN) | (1 << TWINT);
-	while (!(TWCR & (1 << TWINT)));
+	uint8_t retries = 0;
+	
+	while (1) {
+		// Initiate START condition
+		TWCR = (1 << TWSTA) | (1 << TWEN) | (1 << TWINT);
+		
+		// Wait for TWINT flag to be set (START condition transmitted)
+		//if ((TWCR & (1 << TWINT))){
+		//	break;
+		//}
+		
+		// Check for START condition success
+		if ((TWSR & 0xF8) == TW_START) {
+			break; // START condition successful, exit loop
+		}
+		
+		// If START condition failed, increment retries and try again
+		retries++;
+	}
 }
 
 void i2c_stop() {
@@ -45,6 +65,7 @@ uint8_t i2c_read() {
 }
 
 int main() {
+	
 	i2c_init();
 	_delay_ms(500); // Attendre pour laisser les ATmega328P esclaves s'initialiser
 	DDRB |= (1 << PB0) | (1 << PB1);
@@ -52,8 +73,8 @@ int main() {
 	while (1) {
 		// Demander l'état de la fenêtre de l'esclave 1
 		i2c_start();
-		i2c_write((SLAVE_ADDRESS_1 << 1) | 1); // Écrire l'adresse de l'ATmega328P esclave 1 en mode lecture
-		windowState1 = i2c_read();
+		//i2c_write(0x30); // Écrire l'adresse de l'ATmega328P esclave 1 en mode lecture
+		//windowState1 = i2c_read();
 		i2c_stop();
 		
 		// Demander l'état de la fenêtre de l'esclave 2
@@ -63,22 +84,23 @@ int main() {
 		//i2c_stop();
 		
 		// Traiter les données reçues et allumer ou éteindre les LED en fonction de l'état de la fenêtre
-		if (windowState1 == 1) {
-			PORTB |= (1 << PB1);	// Allumer la LED 1 
-			
-			} else {
-			PORTB &= ~(1 << PB1);	// Éteindre la LED 1
-		}
+		//if (windowState1 == 1) {
+		//	PORTB |= (1 << PB1);	// Allumer la LED 1 
+		//	
+		//	} else {
+		//	PORTB &= ~(1 << PB1);	// Éteindre la LED 1
+		//}
 		
-		if (windowState2 == 1) {
+		
 			PORTB |= (1 << PB0);	// Allumer la LED 2 
+			_delay_ms(500);
 			
-			} else {
 			PORTB &= ~(1 << PB0);	// Éteindre la LED 2 
 			
-		}
 		
-		_delay_ms(1000); // Attendre avant de refaire la demande
+		
+		_delay_ms(500); // Attendre avant de refaire la demande
+		
 	}
 
 	return 0;
