e22dbe3a51bd959efbce61415a8b1456bd835958
DJ97320 <jeremy.dalphrase973@gmail.com>
2023-09-17 18:54:57 -0400

    Initial code UART1(Master) + UART2(Slave)

---
+++ Programming/UART_2_Slave/UART_2_Slave/Debug/main.d
@@ -0,0 +1,52 @@
+main.d main.o: .././main.c \
+ d:\logiciel\microchip_studio\avr\avr\include\avr\io.h \
+ d:\logiciel\microchip_studio\avr\avr\include\avr\sfr_defs.h \
+ d:\logiciel\microchip_studio\avr\avr\include\inttypes.h \
+ d:\logiciel\microchip_studio\avr\avr\include\features.h \
+ d:\logiciel\microchip_studio\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\logiciel\microchip_studio\avr\avr\include\stdint.h \
+ d:\logiciel\microchip_studio\avr\avr\include\bits\alltypes.h \
+ d:\logiciel\microchip_studio\avr\avr\include\bits\stdint.h \
+ D:/LOGICIEL/Microchip_Studio/7.0/Packs/atmel/ATmega_DFP/1.7.374/xc8/avr/include/avr/iom328.h \
+ D:/LOGICIEL/Microchip_Studio/7.0/Packs/atmel/ATmega_DFP/1.7.374/xc8/avr/include/avr/iom328p.h \
+ d:\logiciel\microchip_studio\avr\avr\include\avr\portpins.h \
+ d:\logiciel\microchip_studio\avr\avr\include\avr\common.h \
+ d:\logiciel\microchip_studio\avr\avr\include\avr\fuse.h \
+ d:\logiciel\microchip_studio\avr\avr\include\avr\lock.h \
+ d:\logiciel\microchip_studio\avr\avr\include\util\delay.h \
+ d:\logiciel\microchip_studio\avr\avr\include\util\delay_basic.h \
+ d:\logiciel\microchip_studio\avr\avr\include\math.h
+
+d:\logiciel\microchip_studio\avr\avr\include\avr\io.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\avr\sfr_defs.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\inttypes.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\features.h:
+
+d:\logiciel\microchip_studio\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\stdint.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\bits\alltypes.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\bits\stdint.h:
+
+D:/LOGICIEL/Microchip_Studio/7.0/Packs/atmel/ATmega_DFP/1.7.374/xc8/avr/include/avr/iom328.h:
+
+D:/LOGICIEL/Microchip_Studio/7.0/Packs/atmel/ATmega_DFP/1.7.374/xc8/avr/include/avr/iom328p.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\avr\portpins.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\avr\common.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\avr\fuse.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\avr\lock.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\util\delay.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\util\delay_basic.h:
+
+d:\logiciel\microchip_studio\avr\avr\include\math.h:
